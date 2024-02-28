8cc708b70b35f74e1aa05734dfdc0adb16e895a1
sdheeraj <sdheeraj@trainee.nrifintech.comgit config --global user.email sdheeraj@trainee.nrifintech.com>
2024-02-28 18:32:03 +0530

    SericiesDI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessagesService } from './services/messages.service';
+import { Post } from './interfaces/posts.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessagesService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  messages: string[] = [];
+  posts: Post[] = [];
+
+  constructor(private messagesService: MessagesService) {
+    this.messages = messagesService.getMessages();
+  }
+
+  ngOnInit() {
+    // this.messagesService.getPosts().subscribe(response => {
+    //   this.posts = response;
+    // }, (error) => {
+    //   console.log(error);
+    // })
+    this.messagesService.getPosts().subscribe({
+      next: (response) => {
+        
+        this.posts = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
   title = '4.ServicesDI';
 }
---
+++ src/app/services/messages.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../interfaces/posts.interface';
+
+@Injectable({
+  providedIn: 'root'
+
+})
+export class MessagesService {
+  private apiUrl: string = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: HttpClient) {}
+  
+
+  getMessages() {
+    return ['message1', 'message2', 'message3'];
+  }
+
+  getPosts() : Observable<Post[]> {
+    return this.http.get<Post[]>(this.apiUrl);
+  }
+}
